a4e810980cafad859fed32378eb72c8361e83437
L3GALNAME <supak00pa06@gmail.com>
2025-05-13 21:19:09 -0400

    LET ME OUTTTT IM DONE

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let price = 1.87;
+let price = 19.5;
 let cid = [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
@@ -10,6 +10,28 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
+/* let cid = [
+  ['PENNY', 0.4],
+  ['NICKEL', 0],
+  ['DIME', 0],
+  ['QUARTER', 0],
+  ['ONE', 0],
+  ['FIVE', 0],
+  ['TEN', 0],
+  ['TWENTY', 0],
+  ['ONE HUNDRED', 0]
+]; */
+const cashValues = [
+  0.01,
+  0.05,
+  0.10,
+  0.25,
+  1.00,
+  5.00,
+  10.00,
+  20.00,
+  100.00
+];
 let cash = 0;
 
 const cashInput = document.getElementById("cash");
@@ -17,19 +39,62 @@ const changeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
 const change = document.getElementById("change");
 
-cid.forEach((element) => {
-  change.innerHTML += `<p>${element[0]} ${element[1]}</p>`;
-});
+function updateRegister() {
+  change.innerHTML = ``;
+  cid.forEach((element) => {
+    change.innerHTML += `<p>${element[0]} ${element[1]}</p>`;
+  });
+};
+
+updateRegister();
 
 const magicTime = () => {
-  const inputVal = Number(cashInput.value);
+  let inputVal = Number(cashInput.value);
   if (inputVal < price) {
     alert("Customer does not have enough money to purchase the item");
   } else if (inputVal === price) {
     changeDue.textContent = `No change due - customer paid with exact cash`;
   } else {
-    for (let i=cid.length; i>0; i--) {
-      
+    let userChange = Math.round((inputVal-price)*100) / 100;
+    console.log(userChange)
+    let cidCopy = JSON.parse(JSON.stringify(cid));
+    let inDebt = true;
+    changeDue.innerText = `Status: OPEN`;
+    let temp = 0;
+    for (let i=cidCopy.length-1; i>-1; i--) {
+      while (cashValues[i] <= userChange && cidCopy[i][1] >= cashValues[i]) {
+        //console.log(cidCopy[i][1] -= cashValues[i]);
+        userChange -= cashValues[i];
+        userChange = Math.round(userChange*100) / 100;
+
+        cidCopy[i][1] -= cashValues[i];
+        cidCopy[i][1] = Math.round(cidCopy[i][1]*100) / 100;
+        if (cidCopy[i][1] < 0) {
+          inDebt = true;
+          break;
+        }
+        inDebt = false;
+      }
+      cidCopy[i][1] = Math.round(cidCopy[i][1]*100) / 100;
+      if (cidCopy[i][1] != cid[i][1]) {
+        temp = Math.round(Number(eval(`${cid[i][1]} - ${cidCopy[i][1]}`)) * 100) / 100;
+        changeDue.innerText += ` \n${cid[i][0]}: \$${temp}`
+      }
+      console.log(cidCopy[i][1], cashValues[i]);
+      console.log(cid[i][1]);
+    }
+    console.log(inDebt);
+    console.log(userChange);
+    if (userChange > 0) { inDebt = true; };
+    if (inDebt) { 
+      changeDue.innerText = `Status: INSUFFICIENT_FUNDS`;
+    } else {
+      cid = JSON.parse(JSON.stringify(cidCopy));
+      if (!cid.find((element) => element[1] > 0)) {
+        let text = String(changeDue.innerText).replace("OPEN", "CLOSED");
+        changeDue.innerText = `${text}`;
+      }
+      updateRegister();
     }
   }
 };
